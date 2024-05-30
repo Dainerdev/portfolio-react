c4b7db22bb29bc3e2d355a5f26dc34be21c8c18e
Dainerdev <florianarroyo15@gmail.com>
2024-05-29 22:25:31 -0500

    Seccion principal/home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import { FooterComponent } from "./components/footerComponent/FooterComponent";
 import { NavComponent } from "./components/navComponent/NavComponent";
+import { MainSection } from "./components/sections/main/MainSection";
 
 function App() {
   return (
     <div className="App">
       <NavComponent />
+
+      {/* Sections */}
+      <MainSection />
+
+
+
+
+
+
       <FooterComponent />
     </div>
   );
---
+++ src/components/sections/main/MainSection.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./MainSection.css";
+
+export const MainSection = () => {
+  return (
+    <div>
+        <section id="Inicio" class="home ">
+        <div class="home-content">
+            <div class="text">
+                <h1>Dainer Samuel Florian Arroyo<br/>
+                    <span>7502210020</span>
+                </h1>
+                <h2>Mi página web personal.</h2>
+            </div>
+            <div class="img-home">
+                <img src="/foto fondo blanco.jpg" alt="Imagen de Portada"/>
+            </div>
+        </div>
+    </section>
+    </div>
+  )
+}
